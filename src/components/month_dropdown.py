7c8ac1a5beec9f231991051a563fae05da6a30ef
jojo0094 <aungkyawkyaw0094@gmail.com>
2023-02-11 22:45:14 +1300

    Create proper year and month dropdown

--- src/components/month_dropdown.py
+++ src/components/month_dropdown.py
@@ -1,34 +1,86 @@
 import pandas as pd
 from dash import Dash, dcc, html
 from dash.dependencies import Input, Output
+from typing import Dict
 
 from ..data.loader import DataSchema
 from . import ids
 
 
-def render(app: Dash, data: pd.DataFrame) -> html.Div:
-    all_months: list[str] = data[DataSchema.MONTH].tolist()
-    unique_months = sorted(set(all_months))
+# def render(app: Dash, data: pd.DataFrame, data_dict: Dict[str,pd.DataFrame]) -> html.Div:
+
+#     unique_months = []
+#     @app.callback(
+#         Output(ids.MONTH_DROPDOWN, "options"),
+#         [Input(ids.YEAR_DROPDOWN, "value"),]   
+#     )
+#     def update_options(zone: str) -> list[dict]:
+#         filtered_data = data.query("year in @years")
+#         return sorted(set(filtered_data[DataSchema.MONTH].tolist()))
+
+#     @app.callback(
+#         Output(ids.MONTH_DROPDOWN, "value"),
+#         [
+#             Input(ids.YEAR_DROPDOWN, "value"),
+#             Input(ids.SELECT_ALL_MONTHS_BUTTON, "n_clicks"),
+#         ],
+#     )
+#     def select_all_months(years: list[str], _: int) -> list[str]:
+#         filtered_data = data.query("year in @years")
+#         return sorted(set(filtered_data[DataSchema.MONTH].tolist()))
+
+#     return html.Div(
+#         children=[
+#             html.H6("Month"),
+#             dcc.Dropdown(
+#                 id=ids.MONTH_DROPDOWN,
+#                 options=[{"label": month, "value": month} for month in unique_months],
+#                 value=unique_months,
+#                 multi=True,
+#             ),
+#             html.Button(
+#                 className="dropdown-button",
+#                 children=["Select All"],
+#                 id=ids.SELECT_ALL_MONTHS_BUTTON,
+#                 n_clicks=0,
+#             ),
+#         ]
+#     )
+
+def render(app: Dash, data: pd.DataFrame, data_dict: Dict[str,pd.DataFrame]) -> html.Div:
+
+    @app.callback(
+        Output(ids.MONTH_DROPDOWN, "options"),
+        [Input(ids.YEAR_DROPDOWN, "value"),
+        Input(ids.ZONE_DROPDOWN, "value")] 
+    )
+    def update_options(years: list[str], zone: str) -> list[dict]:
+        data = data_dict[zone]
+        filtered_data = data.query("year in @years")
+        unique_months = sorted(set(filtered_data[DataSchema.MONTH].tolist()))
+        return [{"label": month, "value": month} for month in unique_months]
 
     @app.callback(
         Output(ids.MONTH_DROPDOWN, "value"),
         [
             Input(ids.YEAR_DROPDOWN, "value"),
             Input(ids.SELECT_ALL_MONTHS_BUTTON, "n_clicks"),
+            Input(ids.ZONE_DROPDOWN, "value")
         ],
     )
-    def select_all_months(years: list[str], _: int) -> list[str]:
-        print(years)
+    def select_all_months(years: list[str], _: int, zone: str) -> list[str]:
+        data = data_dict[zone]
         filtered_data = data.query("year in @years")
-        return sorted(set(filtered_data[DataSchema.MONTH].tolist()))
+        unique_months = sorted(set(filtered_data[DataSchema.MONTH].tolist()))
+        return unique_months
 
     return html.Div(
         children=[
             html.H6("Month"),
             dcc.Dropdown(
                 id=ids.MONTH_DROPDOWN,
-                options=[{"label": month, "value": month} for month in unique_months],
-                value=unique_months,
+                options=[],
+                value=[],
                 multi=True,
             ),
             html.Button(
--- src/components/year_dropdown.py
+++ src/components/year_dropdown.py
@@ -7,9 +7,52 @@ from ..data.loader import DataSchema
 from . import ids
 
 
-unique_years = ['wah']
+# def render(app: Dash, data: pd.DataFrame, data_dict: Dict[str,pd.DataFrame]) -> html.Div:
+#     unique_years = []
+#     @app.callback(
+#         Output(ids.YEAR_DROPDOWN, "value"),
+#         [Input(ids.SELECT_ALL_YEARS_BUTTON, "n_clicks"),
+#         Input(ids.ZONE_DROPDOWN, "value"),
+#         ]   
+#     )
+#     def select_all_years(_: int, zone: str) -> list[str]:
+#         data = data_dict[zone]
+#         all_years: list[str] = data[DataSchema.YEAR].tolist()
+#         unique_years = sorted(set(all_years), key=int)
+        
+#         return  unique_years
+
+#     return html.Div(
+#         children=[
+#             html.H6("Year"),
+#             dcc.Dropdown(
+#                 id=ids.YEAR_DROPDOWN,
+#                 options=[{"label": year, "value": year} for year in unique_years],
+#                 value=unique_years,
+#                 multi=True,
+#             ),
+#             html.Button(
+#                 className="dropdown-button",
+#                 children=["Select All"],
+#                 id=ids.SELECT_ALL_YEARS_BUTTON,
+#                 n_clicks=0,
+#             ),
+#         ]
+#     )
+
 def render(app: Dash, data: pd.DataFrame, data_dict: Dict[str,pd.DataFrame]) -> html.Div:
     unique_years = []
+    
+    @app.callback(
+        Output(ids.YEAR_DROPDOWN, "options"),
+        [Input(ids.ZONE_DROPDOWN, "value"),]   
+    )
+    def update_options(zone: str) -> list[dict]:
+        data = data_dict[zone]
+        all_years: list[str] = data[DataSchema.YEAR].tolist()
+        unique_years = sorted(set(all_years), key=int)
+        return [{"label": year, "value": year} for year in unique_years]
+
     @app.callback(
         Output(ids.YEAR_DROPDOWN, "value"),
         [Input(ids.SELECT_ALL_YEARS_BUTTON, "n_clicks"),
@@ -20,17 +63,15 @@ def render(app: Dash, data: pd.DataFrame, data_dict: Dict[str,pd.DataFrame]) -> 
         data = data_dict[zone]
         all_years: list[str] = data[DataSchema.YEAR].tolist()
         unique_years = sorted(set(all_years), key=int)
-        
-        return  unique_years
+        return unique_years
 
-    print(unique_years,'unique years *********************************************************************************************************')
     return html.Div(
         children=[
             html.H6("Year"),
             dcc.Dropdown(
                 id=ids.YEAR_DROPDOWN,
                 options=[{"label": year, "value": year} for year in unique_years],
-                value=unique_years,
+                value=[],
                 multi=True,
             ),
             html.Button(
@@ -42,102 +83,4 @@ def render(app: Dash, data: pd.DataFrame, data_dict: Dict[str,pd.DataFrame]) -> 
         ]
     )
 
-# def render(app: Dash, data: pd.DataFrame, data_dict: Dict[str,pd.DataFrame]) -> html.Div:
-#     unique_years = []
-    
-#     @app.callback(
-#         Output(ids.YEAR_DROPDOWN, "options"),
-#         [Input(ids.SELECT_ALL_YEARS_BUTTON, "n_clicks"),
-#          Input(ids.ZONE_DROPDOWN, "value")],
-#     )
-#     def select_all_years(n_clicks, zone: str) -> list[dict]:
-#         data = data_dict[zone]
-#         all_years: list[str] = data[DataSchema.YEAR].tolist()
-#         unique_years = sorted(set(all_years), key=int)
-#         return [{"label": year, "value": year} for year in unique_years]
-    
-#     return html.Div(
-#         children=[
-#             html.H6("Year"),
-#             dcc.Dropdown(
-#                 id=ids.YEAR_DROPDOWN,
-#                 options=[{"label": year, "value": year} for year in unique_years],
-#                 value=unique_years,
-#                 multi=True,
-#             ),
-#             html.Button(
-#                 className="dropdown-button",
-#                 children=["Select All"],
-#                 id=ids.SELECT_ALL_YEARS_BUTTON,
-#                 n_clicks=0,
-#             ),
-#         ]
-#     )
-
-# def render(app: Dash, data: pd.DataFrame, data_dict: Dict[str,pd.DataFrame]) -> html.Div:
-#     unique_years = []
-    
-#     @app.callback(
-#         [Output(ids.YEAR_DROPDOWN, "options"),
-#          Output(ids.SELECT_ALL_YEARS_BUTTON, "n_clicks")],
-#         [Input(ids.SELECT_ALL_YEARS_BUTTON, "n_clicks"),
-#          Input(ids.ZONE_DROPDOWN, "value")],
-#         [State(ids.SELECT_ALL_YEARS_BUTTON, "n_clicks")]
-#     )
-#     def select_all_years(n_clicks, zone: str, current_n_clicks) -> Tuple[list[dict], int]:
-#         data = data_dict[zone]
-#         all_years: list[str] = data[DataSchema.YEAR].tolist()
-#         unique_years = sorted(set(all_years), key=int)
-#         return [{"label": year, "value": year} for year in unique_years], current_n_clicks + 1
-    
-#     return html.Div(
-#         children=[
-#             html.H6("Year"),
-#             dcc.Dropdown(
-#                 id=ids.YEAR_DROPDOWN,
-#                 options=[{"label": year, "value": year} for year in unique_years],
-#                 value=unique_years,
-#                 multi=True,
-#             ),
-#             html.Button(
-#                 className="dropdown-button",
-#                 children=["Select All"],
-#                 id=ids.SELECT_ALL_YEARS_BUTTON,
-#                 n_clicks=0,
-#             ),
-#         ]
-#     )
-
-
-# def render(app: Dash, data: pd.DataFrame, data_dict: Dict[str,pd.DataFrame]) -> html.Div:
-#     unique_years = []
-    
-#     @app.callback(
-#         Output(ids.YEAR_DROPDOWN, "options"),
-#         [Input(ids.SELECT_ALL_YEARS_BUTTON, "n_clicks"),
-#          Input(ids.ZONE_DROPDOWN, "value")]
-#     )
-#     def select_all_years(n_clicks, zone: str) -> list[dict]:
-#         data = data_dict[zone]
-#         all_years: list[str] = data[DataSchema.YEAR].tolist()
-#         unique_years = sorted(set(all_years), key=int)
-#         return [{"label": year, "value": year} for year in unique_years]
-    
-#     return html.Div(
-#         children=[
-#             html.H6("Year"),
-#             dcc.Dropdown(
-#                 id=ids.YEAR_DROPDOWN,
-#                 options=[],
-#                 value=unique_years,
-#                 multi=True,
-#             ),
-#             html.Button(
-#                 className="dropdown-button",
-#                 children=["Select All"],
-#                 id=ids.SELECT_ALL_YEARS_BUTTON,
-#                 n_clicks=0,
-#             ),
-#         ]
-#     )
 
